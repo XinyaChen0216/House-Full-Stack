2ceeeb22ea7f37f08b371d0f874809f1ca73c739
XinyaChen0216 <chen.xinya1@northeastern.edu>
2023-06-17 14:40:32 -0700

    first version of home page with search and list w/o login user

--- src/house/houses/houseItem.js
+++ src/house/houses/houseItem.js
@@ -1,52 +1,94 @@
-// import TuitsStats from "./tuitStats"
 import { useDispatch } from "react-redux";
-// import { deleteTuit } from "../reducers/tuits-reducer";
-import { deleteTuitThunk } from "../services/tuits-thunks";
 import { RxCross1 } from "react-icons/rx";
-import { BsPatchCheckFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa";
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { deleteHouse } from "../reducers/houses-reducer";
 
 const HouseItem = ({ house }) => {
+  const [modalShow, setModalShow] = useState(false);
   const dispatch = useDispatch();
-  const deleteTuitHandler = (id) => {
-    dispatch(deleteTuitThunk(id));
-  };
+  const deletePostHandler = (event, id) => {
+    event.stopPropagation();
+    dispatch(deleteHouse(id));
 
+  };
+  const savePostHandler = (event) => {
+    event.stopPropagation();
+  };
   return (
-    <li className="list-group-item">
-      <div className="row">
-        <div className="card col-6">
-          <img src={`/images/${house.images[0]}`} className="card-img-top" alt="..." />
-          <div className="card-body">
+    <>
+      <li
+        className="list-group-item border border-0 p-0 pb-1 pe-1"
+        style={{ maxWidth: "19rem" }}
+      >
+        <div className="card" onClick={() => setModalShow(true)}>
+          <div className="">
+            <img
+              src={`/images/${house.images[0]}`}
+              className="card-img-top position-relative"
+              alt="..."
+            />
+            <span
+              className="float-end position-absolute top-0 end-0 pe-2 pt-2"
+              onClick={(event) => deletePostHandler(event, house._id)}
+            >
+              <RxCross1 />
+            </span>
+            <span
+              className="float-end position-absolute top-0 start-0 ps-2 pt-2"
+              onClick={(event) => savePostHandler(event)}
+            >
+              <FaHeart className="" />
+            </span>
+          </div>
+          <div className="card-body pb-1">
             <h5 className="card-title">Price: ${house.price}</h5>
             <div className="card-text">
-              <p className="mb-0">{house.address}, {house.city}, {house.state}</p>
+              <p className="mb-0">
+                {house.address}, {house.city}, {house.state}
+              </p>
               <p>{house.overview}</p>
             </div>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
           </div>
         </div>
-      </div>
-    </li>
+      </li>
+      <Modal
+        size="lg"
+        scrollable="true"
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="d-flex justify-content-between w-100">
+            <span>
+              {house.address}, {house.city}, {house.state}
+            </span>
+            <span className="me-3" onClick={(event) => savePostHandler(event)}>
+              <FaHeart className="" />
+            </span>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel data-bs-theme="dark">
+            {house.images.map((image) => (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={`/images/${image}`}
+                  alt=""
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <h3>Overview</h3>
+          <span>{house.overview}</span>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
 export default HouseItem;
-
-// {/* <div className="col-2">
-//                     <img width={70} className="float-end rounded-3" src={`/images/${tuit.image}`} />
-//                 </div> */}
-//                 {/* <div className="col-10">
-//                     <span className="float-end" onClick={() => deleteTuitHandler(tuit._id)}> <RxCross1 /></span>
-//                     <div>
-//                         <b>{tuit.userName}</b>
-//                         <span className="ms-2 me-2" ><BsPatchCheckFill color="blue" /></span>
-//                         <span className="me-2">{tuit.handle} </span>
-//                         {tuit.time}
-//                     </div>
-//                     {/* <div className="fw-bolder">{tuit.topic}</div> */}
-//                     <div>{tuit.tuit}</div>
-//                     {/* <TuitsStats tuit={tuit} /> */}
-//                 </div> */}
--- src/house/houses/houseList.js
+++ src/house/houses/houseList.js
@@ -1,29 +1,22 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { findTuitsThunk } from "../services/tuits-thunks";
-import HouseItem from "./houseItem"
+import HouseItem from "./houseItem";
 
 const HouseList = () => {
-    const { houses, loading } = useSelector(state => state.houses)
-    const dispatch = useDispatch();
-    // useEffect(() => {
-    //     dispatch(findTuitsThunk())
-    // }, [])
-    return (
-        <ul className="list-group">
-            {loading &&
-                <li className="list-group-item">
-                    Loading...
-                </li>
-            }
-            {
-                houses.map(house =>
-                    <HouseItem house={house}
-                    />
-                )
-            }
-        </ul>
-    );
-}
+  const { houses, loading } = useSelector((state) => state.houses);
+  const dispatch = useDispatch();
+  // useEffect(() => {
+  //     dispatch(findTuitsThunk())
+  // }, [])
+  return (
+    <ul className="list-group d-flex flex-row flex-wrap justify-content-start">
+      {loading && <li className="list-group-item">Loading...</li>}
+      {houses.map((house) => (
+        <HouseItem house={house} />
+      ))}
+    </ul>
+  );
+};
 
 export default HouseList;
