004cfe4d44d09ad9bf2dfdcb8bece35197a9014a
imcarolll <jiajialu212@gmail.com>
2023-06-24 12:25:20 -0700

    completed follow and unfollow button

--- src/house/user/other-profile-screen.js
+++ src/house/user/other-profile-screen.js
@@ -6,6 +6,7 @@ import AgentPublicInfo from "./agent-public";
 import BuyerSellerPublicInfo from "./buyer-seller-public";
 import ProfileScreen from "./profile-screen";
 import { updateUserThunk } from "../services/auth-thunks";
+import {RiUserFollowFill} from "react-icons/ri";
 
 function OtherProfileScreen() {
   const { currentUser, requestedUser } = useSelector((state) => state.user);
@@ -14,7 +15,7 @@ function OtherProfileScreen() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const followUserHandler = async (event) => {
+  const followUserHandler = async () => {
     if (currentUser && requestedUser){
       let currFollowing = [...currentUser.following];
       const followIdx = currFollowing.findIndex((curr) => curr === requestedUser._id);
@@ -28,13 +29,50 @@ function OtherProfileScreen() {
         ...currentUser,
         following: [...currFollowing],
       };
+      
+      let reqFollowers = requestedUser.followers;
+      reqFollowers = reqFollowers + 1;
+      let updatedReqFollowers = {
+        ...requestedUser,
+        followers: reqFollowers,
+      };
+      await dispatch(updateUserThunk(updatedReqFollowers));
+      console.log(requestedUser.followers)
       await dispatch(updateUserThunk(newFollowing));
+      
     } else {
       await dispatch(loginThunk());
       navigate("/house/login")
     }
   }
 
+  const unFollowUserHandler = async () => {
+    if (currentUser && requestedUser){
+      let currFollowing = [...currentUser.following];
+      //const followIdx = currFollowing.findIndex((curr) => curr === requestedUser._id);
+      // if (followIdx >= 0) {
+      //   currFollowing.splice(followIdx, 1);
+      // } else {
+      currFollowing.pop(requestedUser._id);
+      // }
+      let newFollowing = {
+        ...currentUser,
+        following: [...currFollowing],
+      };
+      let reqFollowers = requestedUser.followers;
+      reqFollowers = reqFollowers - 1;
+      console.log(reqFollowers);
+      let updatedReqFollowers = {
+        ...requestedUser,
+        followers: reqFollowers,
+      };
+      await dispatch(updateUserThunk(updatedReqFollowers));
+      await dispatch(updateUserThunk(newFollowing));
+    } else {
+      await dispatch(loginThunk());
+      navigate("/house/login")
+    }
+  }
   
   
   useEffect(()=>{
@@ -44,22 +82,33 @@ function OtherProfileScreen() {
   
   return (
     <div>
+      <div>
       <h1> {reqProfile.first_name}'s Profile</h1>
-      
+      {( currentUser.following.includes(requestedUser._id) && (<RiUserFollowFill className = "text-primary"/>))}
+      </div>
+      <div>
       {reqProfile.role === "agent" && AgentPublicInfo(reqProfile)}
       {reqProfile.role === "buyer" && BuyerSellerPublicInfo(reqProfile)}
       {reqProfile.role === "seller" && BuyerSellerPublicInfo(reqProfile)}
-      
-      <div >
-      <button className="btn btn-primary mt-2"
+      </div>
+      <div>
+      {( !currentUser.following.includes(requestedUser._id) && ( <button className="btn btn-primary mt-2"
         onClick={(event) => {
           followUserHandler(event);
-        }}>Follow</button>
+        }}>Follow</button>))}
+
+      {( currentUser.following.includes(requestedUser._id) && ( <button className="btn btn-primary mt-2"
+        onClick={(event) => {
+          unFollowUserHandler(event);
+        }}>Unfollow</button>))}
+     
       <span> </span>
+      
       <button className="btn btn-primary mt-2"
         onClick={async() => {
           navigate("/house/home");
         }}>Finish</button>
+      
       </div>
     </div>
   ); 
