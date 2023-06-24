fdcaf7d6efd22382cae5a38e086cd3a5c3b68ac7
XinyaChen0216 <chen.xinya1@northeastern.edu>
2023-06-23 23:19:33 -0700

    add reduc persist to keep login user

--- src/house/index.js
+++ src/house/index.js
@@ -11,47 +11,66 @@ import List from "./lists-screen";
 import More from "./more-screen";
 import WhoToFollowList from "./who-to-follow-list";
 import whoReducer from "./reducers/who-reducer";
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import houseReducer from "./reducers/houses-reducer";
 import authReducer from "./reducers/auth-reducer";
 import RegisterScreen from "./user/register-screen";
 import LoginScreen from "./user/login-screen";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = configureStore(
-    { reducer: { who: whoReducer, houses: houseReducer, user: authReducer } });
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: {
+    who: whoReducer,
+    houses: houseReducer,
+    user: persistedAuthReducer,
+  },
+  middleware: [thunk],
+});
+
+const persistor = persistStore(store);
 
 function House() {
-    return (
-        <Provider store={store}>
-            <div>
-                <div className="row">
-                    <div className="col-xl-2 col-1">
-                        <NavigationSidebar />
-                    </div>
-                    <div className="col-xl-6 col-lg-7 col-11">
-                        <Routes>
-                            <Route path="/home" element={<HomeScreen />} />
-                            <Route path="/explore" element={<ExploreScreen />} />
-                            <Route path="/bookmarks" element={<BookmarksScreen />} />
-                            <Route path="/profile" element={<ProfileScreen />} />
-                            <Route path="/profile/*" element={<OtherProfileScreen />} />
-                            <Route path="/notifications" element={<Notifications />} />
-                            <Route path="/messages" element={<Messages />} />
-                            <Route path="/lists" element={<List />} />
-                            <Route path="/more" element={<More />} />
-                            <Route path="/login" element={<LoginScreen />} />
-                            <Route path="/register" element={<RegisterScreen />} />
-                            {/* <Route path="/profile" element={<ProfileScreen />} /> */}
-
-                        </Routes>
-                    </div>
-                    <div className="col-4 d-none d-md-none d-lg-block">
-                        <WhoToFollowList />
-                    </div>
-                </div>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div>
+          <div className="row">
+            <div className="col-xl-2 col-1">
+              <NavigationSidebar />
             </div>
-        </Provider>
-    );
+            <div className="col-xl-6 col-lg-7 col-11">
+              <Routes>
+                <Route path="/home" element={<HomeScreen />} />
+                <Route path="/explore" element={<ExploreScreen />} />
+                <Route path="/bookmarks" element={<BookmarksScreen />} />
+                <Route path="/profile" element={<ProfileScreen />} />
+                <Route path="/profile/*" element={<OtherProfileScreen />} />
+                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/lists" element={<List />} />
+                <Route path="/more" element={<More />} />
+                <Route path="/login" element={<LoginScreen />} />
+                <Route path="/register" element={<RegisterScreen />} />
+                {/* <Route path="/profile" element={<ProfileScreen />} /> */}
+              </Routes>
+            </div>
+            <div className="col-4 d-none d-md-none d-lg-block">
+              <WhoToFollowList />
+            </div>
+          </div>
+        </div>
+      </PersistGate>
+    </Provider>
+  );
 }
 export default House;
